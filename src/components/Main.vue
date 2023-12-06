57d0262242f0c87bc5412ad95b8c2402e6c28a42
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-06 16:42:19 +0100

    [ADD]: partials ProjecCard

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,30 +1,33 @@
 <script>
+import ProjectCard from './partials/ProjectCard.vue';
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    ProjectCard
+  }
 }
 </script>
 
 <template>
   <main class="container">
-    
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Name</h5>
-    <p class="card-text">Type</p>
-    <p class="card-text">Technologies</p>
-    <p class="card-text">Description</p>
-    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-  </div>
-</div>
-    
 
+    <div class="row p-5">
+      <div class="col d-flex  flex-wrap ">
+
+        <ProjectCard/>
+
+      </div>
+    </div>
+
+
+    
 
   </main>
 </template>
 
 <style lang="scss" scoped>
 .container {
-  margin-top: 20px; 
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/partials/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'ProjectCard'
+}
+</script>
+
+<template>
+  <div class="card m-2" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Name</h5>
+      <p class="card-text">Type</p>
+      <p class="card-text">Technologies</p>
+      <p class="card-text">Description</p>
+      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
