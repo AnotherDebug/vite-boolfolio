b04295182861895ef14b5e7fde7504f0b1d6ce23
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-06 15:59:45 +0100

    [ADD]: getApi

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script>
+
 import Header from "./components/Header.vue"
 import Main from "./components/Main.vue"
+import axios from "axios"
+import { store } from "./data/store"
+
 export default {
   name: "App",
   components: {
     Header,
     Main,
+  },
+  methods:{
+    getApi() {
+      axios.get(store.apiUrl + 'projects')
+      .then(res => {
+        console.log(res.data);
+      });
+    },
+  },
+  mounted() {
+    this.getApi();
   }
 }
 </script>
