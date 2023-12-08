047522b7920c42c969c89e6a6b124f0bf7692671
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-08 10:49:03 +0100

    [ADD]: cdn fontawesome [UPDATE]: card

--- src/components/partials/ProjectCard.vue
+++ src/components/partials/ProjectCard.vue
@@ -8,22 +8,17 @@ export default {
 </script>
 
 <template>
-  <div class="card m-2" style="width: 18rem;">
- 
-      <img :src="!!item?.image ? `http://127.0.0.1:8000/storage/` + item.image : 'https://via.placeholder.com/200x200'" 
-        	  class="card-img-top" 
-            :alt="item.image_original_name">
+  <div class="card m-auto mb-5  bg-body-secondary " style="width: 25rem;">
 
     <div class="card-body">
       <h5 class="card-title"><strong>Project:</strong> {{ item.name }}</h5>
       <p class="card-text"><strong>Type:</strong> {{ item.type.name }}</p>
-      <p 
+      <!-- <p 
         class="card-text"><strong>Technologies:</strong> <span 
           v-for="technology in item.technologies"
           :key="technology.id">{{ item.technologies.map(tech => tech.name).join(', ') }}</span>
-      </p>
-      <p class="card-text"><strong>Description:</strong> <span v-html="item.description"></span></p>
-      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+      </p> -->
+      <a href="#" class="btn btn-primary">Details <i class="fa-solid fa-square-arrow-up-right fa-rotate-90"></i></a>
 
     </div>
   </div>
